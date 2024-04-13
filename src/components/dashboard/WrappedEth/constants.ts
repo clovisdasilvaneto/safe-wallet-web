08fccd8c3cd35281b745d7f4bd5a00d5af7de3f9
Clovis Neto <clovisdasilvaneto@gmail.com>
2024-04-14 01:54:17 +0200

    feat: create hook to fetch eth and weth balances

---
+++ src/components/dashboard/WrappedEth/constants.ts
@@ -0,0 +1,3 @@
+export const WETH_ADDRESS = process.env.NEXT_PUBLIC_WETH_ADDRESS || '0x7b79995e5f793a07bc00c21412e50ecae098e7f9'
+
+export const EMPTY_VALUE = '0'
---
+++ src/components/dashboard/WrappedEth/useWrappedBalances.ts
@@ -0,0 +1,54 @@
+import useAsync from '@/hooks/useAsync'
+import useBalances from '@/hooks/useBalances'
+import useChainId from '@/hooks/useChainId'
+import { safeFormatUnits } from '@/utils/formatters'
+import { TokenType } from '@safe-global/safe-apps-sdk'
+import { useMemo } from 'react'
+import { WETH_ADDRESS, EMPTY_VALUE } from './constants'
+import { getWeb3ReadOnly } from '@/hooks/wallets/web3'
+import useSafeAddress from '@/hooks/useSafeAddress'
+import { Interface } from 'ethers'
+
+const fetchWETHBalance = async (safeAddress: string): Promise<string> => {
+  try {
+    const tokenInterface = new Interface(['function balanceOf(address _owner) public view returns (uint256 balance)'])
+    const web3ReadOnly = getWeb3ReadOnly()
+
+    if (!web3ReadOnly) return EMPTY_VALUE
+
+    const wethHexBalance = await web3ReadOnly.call({
+      to: WETH_ADDRESS,
+      data: tokenInterface.encodeFunctionData('balanceOf', [safeAddress]),
+    })
+
+    if (!wethHexBalance) return EMPTY_VALUE
+
+    const balance = BigInt(wethHexBalance).toString()
+
+    return safeFormatUnits(balance, 18)
+  } catch (err) {
+    throw Error(`Error fetching Safe Token balance:  ${err}`)
+  }
+}
+
+const useWrappedBalances = () => {
+  const { balances, loading } = useBalances()
+  const chainId = useChainId()
+  const address = useSafeAddress()
+
+  const ethBalance = useMemo(
+    () =>
+      balances.items.reduce((acc, balance) => {
+        if (balance.tokenInfo.type !== TokenType.NATIVE_TOKEN) return acc
+
+        return safeFormatUnits(balance.balance, balance.tokenInfo.decimals)
+      }, EMPTY_VALUE),
+    [balances],
+  )
+
+  const [wethBalance, loadingBalance] = useAsync(() => fetchWETHBalance(address), [chainId, ethBalance, address])
+
+  return { ethBalance, wethBalance, loading: loading || loadingBalance }
+}
+
+export default useWrappedBalances
